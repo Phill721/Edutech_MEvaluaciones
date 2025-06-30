821ae2ae36330bdb400e343fbcc2cc4aa0dcd7cc
Phillipe <ge.bonhomme@duocuc.cl>
2025-06-29 22:41:11 -0400

    SwaggerOAS implementado

--- src/main/java/com/Edutech/Evaluaciones/Controller/EvaluacionController.java
+++ src/main/java/com/Edutech/Evaluaciones/Controller/EvaluacionController.java
@@ -17,24 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Edutech.Evaluaciones.Model.Evaluacion;
 import com.Edutech.Evaluaciones.Service.EvaluacionService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/evaluaciones")
+@Tag(name = "Evaluaciones", description = "Operaciones relacionadas con las instancias de Evaluacion")
 public class EvaluacionController {
     @Autowired
     private EvaluacionService evaluacionService;
 
     @PostMapping
+    @Operation(summary = "Crear una evaluacion", description = "Permite crear una instancia de evaluacion y guardarla en la base de datos.")
     public ResponseEntity<Evaluacion> crearEvaluacion(@RequestBody Evaluacion evaluacion){
         Evaluacion newEvaluacion = evaluacionService.crearEvaluacion(evaluacion);
         return ResponseEntity.status(HttpStatus.CREATED).body(newEvaluacion);
     }
 
     @GetMapping
+    @Operation(summary = "Listar evaluaciones", description = "Esta funcion permite listar todas las instancias de evaluaciones registradas en la base de datos.")
     public List<Evaluacion> listarEvaluaciones(){
         return evaluacionService.listar();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar instancia por ID", description = "Esta funcion permite buscar una instancia especifica de Evaluacion a travez del ID de la misma.")
     public ResponseEntity<Evaluacion> buscarporID(@PathVariable int id){
         return evaluacionService.buscarporID(id)
             .map(ResponseEntity::ok)
@@ -42,6 +49,7 @@ public class EvaluacionController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar instancia", description = "Esta funcion permite actualizar los datos una instancia de Evaluacion ya existente.")
     public ResponseEntity<Evaluacion> actualizarEvaluacion(@PathVariable int id, @RequestBody Evaluacion nuevaEvaluacion){
         return evaluacionService.actualizarEvaluacion(id, nuevaEvaluacion)
             .map(ResponseEntity::ok)
@@ -49,6 +57,7 @@ public class EvaluacionController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar instancia", description = "Esta funcion permite eliminar una instancia existente de Evaluacion, de la base de datos.")
     public ResponseEntity<Void> eliminarEvaluacion(@PathVariable int id){
         evaluacionService.eliminarEvaluacion(id);
         return ResponseEntity.noContent().build();
--- src/main/java/com/Edutech/Evaluaciones/Controller/PreguntaController.java
+++ src/main/java/com/Edutech/Evaluaciones/Controller/PreguntaController.java
@@ -17,25 +17,32 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Edutech.Evaluaciones.Model.Pregunta;
 import com.Edutech.Evaluaciones.Service.PreguntaService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/preguntas")
+@Tag(name = "Preguntas", description = "Operaciones relacionadas con las instancias de Pregunta")
 public class PreguntaController {
     @Autowired
     private PreguntaService preguntaService;
 
 
     @PostMapping
+    @Operation(summary = "Crear una pregunta", description = "Permite crear una instancia de Pregunta y guardarla en la base de datos.")
     public ResponseEntity<Pregunta> crearPregunta(@RequestBody Pregunta pregunta){
         Pregunta nuevPregunta = preguntaService.crearPregunta(pregunta);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevPregunta);
     }
 
     @GetMapping
+    @Operation(summary = "Listar preguntas", description = "Esta funcion permite listar todas las instancias de preguntas registradas en la base de datos.")
     public List<Pregunta> listaPreguntas(){
         return preguntaService.listar();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar instancia por ID", description = "Esta funcion permite buscar una instancia especifica de Pregunta a travez del ID de la misma.")
     public ResponseEntity<Pregunta> buscarxid(@PathVariable int id){
         return preguntaService.buscarxid(id)
             .map(ResponseEntity::ok)
@@ -43,6 +50,7 @@ public class PreguntaController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar instancia", description = "Esta funcion permite actualizar los datos una instancia de Pregunta ya existente.")
     public ResponseEntity<Pregunta> actualizarPregunta(@PathVariable int id, @RequestBody Pregunta nuevaPregunta){
         return preguntaService.actualizarPregunta(id, nuevaPregunta)
             .map(ResponseEntity::ok)
@@ -50,12 +58,14 @@ public class PreguntaController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar instancia", description = "Esta funcion permite eliminar una instancia existente de Pregunta, de la base de datos.")
     public ResponseEntity<Void> eliminarPregunta(@PathVariable int id){
         preguntaService.eliminarPregunta(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}/validar/{respuesta}")
+    @Operation(summary = "Validar respuesta de pregunta", description = "Esta funcion permite validar si la respuesta de una pregunta es correcta o no mediante un boolean. Si el parametro de Respuesta coincide con el numero de respuesta correcta de la pregunta, entonces esta devolverá un TRUE.")
     public ResponseEntity<Boolean> validar(@PathVariable int id, @PathVariable int respuesta){
         boolean esCorrecto = preguntaService.validarRespuesta(id, respuesta);
         return ResponseEntity.ok(esCorrecto);
